ac6e17c9bd3a964aca34412eee2c4350fa01a062
Alexander <o.komarnytskyi@polytech.software>
2024-11-21 00:01:21 +0200

    Added grpc implementation, user and organization grpc services

---
+++ GrpcHttpService.Grpc/Services/OrganizationGrpcService.cs
@@ -0,0 +1,54 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using GrpcHttpService.BusinessLogic.Interfaces.Service;
+using GrpcHttpService.Grpc.Protos;
+
+namespace GrpcHttpService.Grpc.Services
+{
+    /// <summary>
+    /// Grpc service for organizations
+    /// </summary>
+    public class OrganizationGrpcService : OrganizationService.OrganizationServiceBase
+    {
+        private readonly IOrganizationService _organizationService;
+
+        public OrganizationGrpcService(IOrganizationService organizationService)
+        {
+            _organizationService = organizationService;
+        }
+
+        public override async Task<CreateOrganizationResponse> CreateOrganization(CreateOrganizationRequest request, ServerCallContext context)
+        {
+            var organization = await _organizationService.CreateOrganizationAsync(request.Name, request.Address);
+            return new CreateOrganizationResponse { Id = organization.Id.ToString() };
+        }
+
+        public override async Task<GetOrganizationByIdResponse> GetOrganizationById(GetOrganizationByIdRequest request, ServerCallContext context)
+        {
+            var organization = await _organizationService.GetOrganizationByIdAsync(Guid.Parse(request.Id));
+            if (organization == null)
+                throw new RpcException(new Status(StatusCode.NotFound, "Organization not found"));
+
+            return new GetOrganizationByIdResponse
+            {
+                Id = organization.Id.ToString(),
+                Name = organization.Name,
+                Address = organization.Address,
+                CreatedAt = organization.CreatedAt.ToString("O"),
+                UpdatedAt = organization.UpdatedAt.ToString("O")
+            };
+        }
+
+        public override async Task<Empty> DeleteOrganization(DeleteOrganizationRequest request, ServerCallContext context)
+        {
+            await _organizationService.DeleteOrganizationAsync(Guid.Parse(request.Id));
+            return new Empty();
+        }
+
+        public override async Task<Empty> RestoreOrganization(RestoreOrganizationRequest request, ServerCallContext context)
+        {
+            await _organizationService.RestoreOrganizationAsync(Guid.Parse(request.Id));
+            return new Empty();
+        }
+    }
+}
---
+++ GrpcHttpService.Grpc/Services/UserGrpcService.cs
@@ -0,0 +1,50 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using GrpcHttpService.BusinessLogic.Interfaces.Service;
+using GrpcHttpService.Grpc.Protos;
+
+namespace GrpcHttpService.Grpc.Services
+{
+    /// <summary>
+    /// Grpc service for users.
+    /// </summary>
+    public class UserGrpcService(IUserService userService) : UserService.UserServiceBase
+    {
+        private readonly IUserService _userService = userService;
+
+        public override async Task<CreateUserResponse> CreateUser(CreateUserRequest request, ServerCallContext context)
+        {
+            var user = await _userService.CreateUserAsync(request.Name, request.Username, request.Email);
+            return new CreateUserResponse { Id = user.Id.ToString() };
+        }
+
+        public override async Task<GetUserByIdResponse> GetUserById(GetUserByIdRequest request, ServerCallContext context)
+        {
+            var user = await _userService.GetUserByIdAsync(Guid.Parse(request.Id));
+            if (user == null)
+                throw new RpcException(new Status(StatusCode.NotFound, "User not found"));
+
+            return new GetUserByIdResponse
+            {
+                Id = user.Id.ToString(),
+                Name = user.Name,
+                Username = user.Username,
+                Email = user.Email,
+                CreatedAt = user.CreatedAt.ToString("O"),
+                UpdatedAt = user.UpdatedAt.ToString("O")
+            };
+        }
+
+        public override async Task<Empty> DeleteUser(DeleteUserRequest request, ServerCallContext context)
+        {
+            await _userService.DeleteUserAsync(Guid.Parse(request.Id));
+            return new Empty();
+        }
+
+        public override async Task<Empty> RestoreUser(RestoreUserRequest request, ServerCallContext context)
+        {
+            await _userService.RestoreUserAsync(Guid.Parse(request.Id));
+            return new Empty();
+        }
+    }
+}
